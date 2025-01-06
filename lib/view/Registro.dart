a25bbb02fcb7d102dd385845cfbd28518b3a0a51
martinaamonerris <145491223+martinaamonerris@users.noreply.github.com>
2025-01-06 20:12:05 +0100

    Update Registro.dart

--- lib/view/Registro.dart
+++ lib/view/Registro.dart
@@ -2,89 +2,120 @@ import 'package:flutter/material.dart';
 import 'Formulario.dart';
 
 class RegisterView extends StatelessWidget {
-  const RegisterView({Key? key}) : super(key: key);
+  final String email;
+
+  const RegisterView({super.key, required this.email});
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController nameController = TextEditingController();
+    final emailController = TextEditingController(text: email);
+    final nameController = TextEditingController();
+    final passwordController = TextEditingController();
+
+    final formKey = GlobalKey<FormState>();
+
+    String? validatePassword(String? value) {
+      if (value == null || value.isEmpty) {
+        return 'Por favor, ingresa una contraseña.';
+      } else if (value.length < 8) {
+        return 'La contraseña debe tener al menos 8 caracteres.';
+      }
+      return null; // Null indica que no hay errores
+    }
 
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Registrarse',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+          child: Form(
+            key: formKey, // Asocia el formKey al formulario
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Registrarse',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  labelText: 'Nombre',
-                  hintText: 'Ingresa tu nombre',
-                  border: OutlineInputBorder(),
+                const SizedBox(height: 20),
+                // Campo de Nombre
+                TextFormField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Nombre',
+                    hintText: 'Ingresa tu nombre',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, ingresa tu nombre.';
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              const SizedBox(height: 20),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Ingresa tu email',
-                  border: OutlineInputBorder(),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Ingresa tu email',
+                    border: OutlineInputBorder(),
+                  ),
+                  readOnly: true, // Email es solo lectura
                 ),
-              ),
-              const SizedBox(height: 20),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Contraseña',
-                  hintText: 'Ingresa tu contraseña',
-                  border: OutlineInputBorder(),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true, // Para ocultar la contraseña
+                  decoration: const InputDecoration(
+                    labelText: 'Contraseña',
+                    hintText: 'Ingresa tu contraseña',
+                    border: OutlineInputBorder(),
+                    helperText:
+                        'La contraseña debe tener al menos 8 caracteres.',
+                  ),
+                  validator: validatePassword, // Valida la contraseña
                 ),
-              ),
-              const SizedBox(height: 30),
-              Center(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 50,
-                      vertical: 15,
+                const SizedBox(height: 30),
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 15,
+                      ),
+                    ),
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => FormularioView(
+                              nombre: nameController.text,
+                            ),
+                          ),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text(
+                                'Por favor, corrige los errores antes de continuar.'),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text(
+                      'Guardar',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
-                  onPressed: () {
-                    // Navegar al formulario pasando el nombre ingresado
-                    if (nameController.text.isNotEmpty) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => FormularioView(
-                            nombre: nameController.text,
-                          ),
-                        ),
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Por favor, ingresa tu nombre.'),
-                        ),
-                      );
-                    }
-                  },
-                  child: const Text(
-                    'Guardar',
-                    style: TextStyle(color: Colors.white),
-                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
